f43f817b1ea454e1d31ecf0a116147044850fd77
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-07-05 11:23:35 +0300

    HomeWork_1 JAVA to Kotlin

--- app/src/main/java/com/dreamyprogrammer/homeworklesson1/MainActivity.kt
+++ app/src/main/java/com/dreamyprogrammer/homeworklesson1/MainActivity.kt
@@ -1,19 +1,16 @@
-package com.dreamyprogrammer.homeworklesson1;
+package com.dreamyprogrammer.homeworklesson1
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.simple_button).setOnClickListener(v -> {
-            Toast.makeText(this,getString(R.string.hello), Toast.LENGTH_LONG).show();
-        });
-
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.simple_button).setOnClickListener {
+            Toast.makeText(this, getString(R.string.hello), Toast.LENGTH_LONG).show()
+        }
     }
 }
